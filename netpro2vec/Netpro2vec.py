8e71e2c95824cc851790bf3d828c6834948139b4
Maurizio Giordano <maurizio.giordano@cnr.it>
2021-07-13 21:55:58 +0200

    Update Netpro2vec.py

--- netpro2vec/Netpro2vec.py
+++ netpro2vec/Netpro2vec.py
@@ -207,7 +207,7 @@ class Netpro2vec:
 		documents = [ d.words for d in docs[idx]]
 		if steps is None: steps = self.epochs 
 		if alpha is None: alpha = self.learning_rate
-		utils.vprint("Doc2Vec inferring (steps=%d, alpha%f)in progress..."%(steps,alpha), end='', verbose=self.verbose)
+		utils.vprint("Doc2Vec inferring (steps=%d, alpha%f) in progress..."%(steps,alpha), end='', verbose=self.verbose)
 		embedding_list = [self.model.infer_vector(doc,steps=steps,alpha=alpha) for doc in documents]
 		utils.vprint("Done!", verbose=self.verbose)
 		return embedding_list
